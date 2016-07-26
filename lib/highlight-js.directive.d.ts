32a7d20b846181a1c3a62a4207acef42905c1c8f
JayChase <JonathanChase@outlook.com>
2016-07-26 12:56:18 +0900

    bumped version and built

--- lib/highlight-js.directive.d.ts
+++ lib/highlight-js.directive.d.ts
@@ -1,7 +1,7 @@
-import { ElementRef, Renderer, AfterContentInit } from '@angular/core';
+import { ElementRef, AfterContentInit } from '@angular/core';
 export declare class HighlightJsDirective implements AfterContentInit {
     private elementRef;
     useBr: boolean;
-    constructor(elementRef: ElementRef, renderer: Renderer);
+    constructor(elementRef: ElementRef);
     ngAfterContentInit(): void;
 }
