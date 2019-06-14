d050b5afd4a03f03fcda899adbac9a8d7bc62361
JayChase <JonathanChase@outlook.com>
2019-06-14 12:14:38 +0900

    init

---
+++ projects/angular-highlight-js/src/lib/angular-highlight-js.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { AngularHighlightJsComponent } from './angular-highlight-js.component';
+
+@NgModule({
+  declarations: [AngularHighlightJsComponent],
+  imports: [
+  ],
+  exports: [AngularHighlightJsComponent]
+})
+export class AngularHighlightJsModule { }
---
+++ projects/angular-highlight-js/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of angular-highlight-js
+ */
+
+export * from './lib/angular-highlight-js.module';
